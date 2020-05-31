c85e091d419f456b5f318582cff29f6801b7887f
ommmarax <66205421+ommmarax@users.noreply.github.com>
2020-05-31 15:24:34 +0300

    Create omaralhaded.js

---
+++ omaralhaded.js
@@ -0,0 +1,54 @@
+if(location.protocol!='https:'){location.protocol='https:';}
+Tsend_=Tsend;
+reqlikes=1;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك الكتابة عام يتطلب منك الحصول على لايكات'+reqlikes+' '); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+reqlikes=10;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert(' لإ يمكنك تغير صورتك يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+reqlikes=10;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('لا يمكنك حفظ الأعدادات يتطلب منك الحصول على لايكات '+reqlikes+''); return;}else{setprofile_();}}
+
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;/a></div>").insertAfter($(".loginstat"));  },1000);
+$(`<img src="https://chatnsh.com/sico/z1e9l0724610.jpg" class="fr" style="width:100%;margin-right: 1px;">
+<div><marquee direction="right" width="99%" id="asim7" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="2">    اهلاوسهلاا بكم في شات قلبي مشتاق للجوال: وكل عام ونتم بخير وعيدكم مبارك  ●·  </marquee></div>
+<div id="asim10"><a id="asim21" href="" target="" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a>
+<a id="asim21" href="" target="" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف شاغره</a>
+<a id="asim21" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات</a><a id="asim22" href="" target="1" type="1" style=" width: 24%; margin: 2px;" class="btn-as">اشتراكات</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">زخرفة</a>
+<a id="asim22" href="" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">ق.السوبر</a>
+<a id="asim22" href="" target="#" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a>
+`).insertBefore(".nav-tabs")
+$(".nav-tabs").addClass("fl").css("width","100%")
+$(".tc span[class=\"fa fl\"]").addClass("fa-check");
+var nopm=!0,nonot=!0;
+$(".hand.nosel.fl.uzr.border").css({margin:"1px 0",borderRadius:"0px"})
+$(".fa-gear").text(" الضبط").css("width",""); 
+$(`<link rel="stylesheet" href="https://rawcdn.githack.com/ommmarax/chatnsh/4a2efa62683fef63354b8bf870ad3634a827381a/omaralhaded.css">
+</style>
+`).insertBefore("body");
+$('.break.light').css({"background-image":"url(https://chatnsh.com/sico/z1e9l0koou10.jpg)"});
+$("div#tlogins button.btn.btn-primary").click(function(){
+    var myVar = setInterval(function(){ var usmsgw = $(".pmsgc").length;if(usmsgw > 0){ 
+    $(`
+        <div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:white;">
+          <img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url(https://chatnsh.com/pic/1e9ktr7tm10.png);" class="fl fitimg hand u-pic    ">
+          <div class="uzr fl" style="padding:0px;width:80%">
+            <div style="padding:0px;width:100%;" class="fl">
+              <img class="fl u-ico" alt="" src="https://chatnsh.com/sico/z1e8lp8j5510.gif">
+              <span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">شات  قـلبــي مشــتآق</span>
+            </div>
+            <br>
+            <div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg   break  fl">
+            <div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg   break  fl"> مرحبا بك <h1 style="display: inline-block;color: red;">`+ getuser(myid).topic+ ` </h1> نـورتنا يسعدنا تواجدك معنا ونتمنى لك وقت ممتع <div> </div>
+        </div></div>
+    `).appendTo('div#d2')
+    clearInterval(myVar);
+    
+    }else{console.log(usmsgw)} }, 2000);
+    })
+(document.getElementById("tbox").placeholder = "اكتب هنا مع اصدقائك ");
+$(`<center><div><div width="75.5%" style="background-color: #b7c3c5; color: #BF4A47;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;" ><font style="background-color: #fff;border-radius: 0px 25px 0px 25px;font-family: 'Cairo', sans-serif, FontAwesome;padding: 2px 10px 2px 10px; margin:0px 25px 0px 25px;" >اهلا وسهلا بكم في شات  قــلبــي مشــتآق    </font></div></div><center>`).insertBefore('#d2');
