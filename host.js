41abe3b684a1034b015665f44a6c3f4a934cc198
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-09-05 11:49:31 +0300

    Add files via upload

---
+++ host.js
@@ -0,0 +1,60 @@
+$(`<img style="margin-left: -1px;width:101%; margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://alhaded-1998.000webhostapp.com/whost.1998/banner-host.jpg" > <a id="jfa44"target="_blank"type="a"rel="nofollow"href=""class="label-primary">برمجة جوال هوست</a></div></div><center>
+<a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a></div></div>
+<a id="asim99" href=""><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i></a> 
+     <div id="asim1">
+      <a id="asim2" target="_blank" type="a" href="rules.html" style="border-radius: 0 10% 0 0; border-right: 2px solid#a6a6a6;" class="btn minix btn-primary">القوانين</a>
+      <a id="asim2" target="_blank" type="a" href="sub.html" style="border-radius: 0%; border-right: 1px solid#f5f8fa;" class="btn minix btn-primary">الاشتراكات</a>
+      <a id="asim10" target="_blank" type="a" href="contact.html" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">اتصل بنا</a>
+      <a id="asim10" target="_blank" type="a" href="ruless.html" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">الراديو</a>
+      <a id="asim10" target="_blank" type="a" href="mobile.html " style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">تطبيق</a>
+      <a id="asim10" target="_blank" type="a" href="Shortcut.html" style="border-radius: 0 0 0 10%; border-left: 2px solid#a6a6a6; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">اختصارات</a> 
+      <a id="hack2" target="_blank" type="a" href="#" style="border-radius: 0 0 10% 0; border-right: 2px solid#a6a6a6; border-top: 1px solid#f5f8fa" class="btn minix btn-primary">المدونة</a>
+      <a id="hack2" target="_blank" type="a" href="https://www.abraj2015.com/" style="border-radius: 0%; border-right: 1px solid#f5f8fa; border-top: 1px solid#f5f8fa;" class="btn minix btn-primary">الآبراج</a> 
+      <center></center>
+     </div>
+    </center>`).insertBefore(".nav-tabs")
+
+$(".ae.label.label-primary.fa.fa-gear").text(' ضبط');
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="https://www.sudfachat.com/sico/1580237645774.png" width="70%" style="">`)
+$('#d0').append(`
+<label title="المحادثات الجماعية" href="#" onclick="$('.pnhead').text($(this).attr('title'));setTimeout(function(){$('#grubesAll').scrollTop(0);},100);$('.dpnl').show();hl($('.btnClAlGr'), 'primary');" data-toggle="tab" data-target="#grubesAll" style="font-weight: 100 !important;" class="ae fa label label-primary fa-users btnClAlGr"> &nbsp;</label>`);
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(` </div><center>
+`).appendTo("#mic"),clearInterval(b)):console.log(c)},1)}),$("div#tlogins button.btn.btn-primary").click(function(){setTimeout(function(){refreshonline()},0)}),setInterval(function(){var a=$("#dpnl");0<a.length&&a.remove()},0),$(".dpnl").append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><center></div><center></div>`)
+eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('$(\'#b\').c(`<1 2="\\3\\4\\5\\d\\e \\f \\4\\5\\3\\g\\h\\i"j="#"k="$(\'.l\').m($(6).n(\'2\'));\no($(6),\'7\');\np(q()\n	{\n	$(\'#8\').r(0);\n}\n,0);\n$(\'.s\').t();\n"9-u="v"9-w="#8"x="y a 1 1-7 a-z-A">&B;</1>`);',38,38,'|label|title|u0645|u0627|u0644|this|primary|mic|data|fa|d0|append|u0643|u064a|u002d|u0648|u0642|u0639|href|onclick|pnhead|text|attr|hl|setTimeout|function|scrollTop|dpnl|show|toggle|tab|target|class|ae|volume|up|nbsp'.split('|'),0,{}))
+$(`<div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="jfa99">♥ ||  الفجر لـلـجــوال || ♥</font> ● · ‫أفضل سوبره لهذا الأسبوع · ● <font class="jfa99" color="#727A54">&nbsp;قريباً ♥ &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="jfa88" color="#ff9900">&nbsp;قريباً &nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="jfa88" color="#ff00ff">&nbsp; قريباً  &nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="jfa88" color="#5CAD9A">&nbsp; قريباً  &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="jfa88" color="#800000">&nbsp; قريباً &nbsp;</font> ● · ‫أفضل أدارية لهذا الأسبوع · ● <font class="jfa88" color="#417570">&nbsp; قريباً  &nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات&nbsp;
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<link rel="stylesheet" href="https://alhaded-1998.000webhostapp.com/whost.1998/host.css"></style>`).insertBefore("body")
+
+
+$("div#tlogins button.btn.btn-primary").click(function()
+	{
+	var c=setInterval(function()
+		{
+		var a=$(".pmsgc").length;
+		0<a?($(`<img style="height: 99.3%;
+																				width:99%;
+																				 margin-top: 2px;
+																				pointer-events:none;
+																				margin-bottom: 5px;
+																				"src=""></div><center>`).appendTo("div#chats"),clearInterval(c)):console.log(a)
+	}
+	,1)
+}
+);
+
+
+ if(location.protocol!='https:'){location.protocol='https:';}
+  
+$(".dpnl").css('width','310px');
+
+
+ 
+$( "div#l1 button.btn.btn-primary" ).bind( "click", function() {
+alert( "اهلا وسهلا بكم في شات الفجر للجوال ❤️ سجل عضوية تحصل على 1500 لايك وزخرفة :: طريقة التسجيل اضغط على خيار (تـسجيـل )  (اكتب اسمك المستعار + كلمة المرور ) وبعدها اضغط دخول" );
+});
+$ ( ` <center> <div style =" width: 100?؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample">- منــورينَ جميعاً : شآت الفجر -</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+$(`<a href="قيد الـتطوير" target="_blank" id="as0" class="border"> محادثة جماعية</a>
+`)['insertBefore']('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg')
