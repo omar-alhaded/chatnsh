d507c926baa0791675d25a74b1f4c8d84ab34398
omar-alhaded <66205421+omar-alhaded@users.noreply.github.com>
2020-09-23 14:30:30 +0300

    Create capten.js

---
+++ capten.js
@@ -0,0 +1,43 @@
+'https:' != location['protocol'] && (location['protocol'] = 'https:'), $(`${'<img style="width:100%;    margin-top: 0px;pointer-events:none;margin-bottom: 0px;" src="https://www.up-00.com/i/00192/f3rpogxx2cb1.gif\x0D\x0A\x0D\x0A" >\x0D\x0A<div id="asim13"><a id="asim21" href="contact.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اتصل بنا</a>\x0D\x0A<a id="asim21" href="rules.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">القوانين</a>\x0D\x0A<a id="asim21" href="Shortcut.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">اختصارات </a>\x0D\x0A<a id="asim21" href="https://blog.jfa-w.net/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">المدونة</a>\x0D\x0A<a id="asim22" href="mobile.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">التطبيق</a>\x0D\x0A<a id="asim22" href="sub.html" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الآشتراكات</a>\x0D\x0A<a id="asim22" href="https://www.abraj2015.com/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">الأبراج</a>\x0D\x0A<a id="asim22" href="https://www.wadhefa.com/" target="_blank" type="button" style=" width: 24%; margin: 2px;" class="btn-as">وظائف</a></div></div><center>'}`)['insertBefore']('.nav-tabs'), $('.nav-tabs')['addClass']('fl')['css']('width', '100%'), $('.tc span[class="fa fl"]')['addClass']('fa-check');
+var nopm = !0
+    , nonot = !0;
+$('.hand.nosel.fl.uzr.border')['css']({ margin: '1px 0', borderRadius: '0px' }), $(`${'<div><marquee direction="right" id="as43"scrollamount="3">اهلا وسهلا بكم\x0D\x0A\x0D\x0A <font class="a">   في شات جفى للجوال </font>  نهني أنفسنا بحلول اليوم الوطني المجيد لأنه يحي بداخلنا شدة الانتماء إلى بلدنا الحبيب واعتزازً بوطننا.\x0D\x0A\x0D\x0Aأدامك الله يا وطني شامخًا رافعًا رأسك بأولادك.&nbsp;</marquee><div>'}`)['insertBefore']('div#tlogins .lonline.light.break'), $('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+var _0xb650 = ['maxlength', '500', 'attr', 'textarea#tbox.fl.corner.tbox.filw'];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]);
+var _0xb650 = ['maxlength', '12', 'attr', 'div#tlogins input'];
+$(_0xb650[3])[_0xb650[2]](_0xb650[0], _0xb650[1]), $('div#tlogins button.btn.btn-primary')['click'](function () {
+    var _0x5fb3x4 = setInterval(function () {
+        var _0x5fb3x5 = $('.pmsgc')['length'];
+        0 < _0x5fb3x5 ? ($(`${'<center><iframe width="390" height="140" src="https://www.youtube.com/embed/W4am_7gRxG0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe><center><center><div style=" width: 100%;height: auto!important; "  id="as" margin: 2px;" class="fa fa-heartbeat btn btn-danger" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; دأم عزك يأوطن ~> (اضغط هنا)&nbsp;</div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>وطني أرجو العذر إنْ خانتني حروفي وأرجو العفو إن أنقصت قدراً فما أنا إلّا عاشقٌ حاول أنْ يتغنى بحبِّ هذا الوطن. فكم أنا فخورٌ بانتماني إليك أيّها الوطن </p><jpg="" jpg=""><a href="#"><img src="https://i.top4top.io/p_1724d6a4u1.gif"style="width:99%;\x0D\x0Amargin-top: 30px;\x0D\x0A"></div><center>'}`)['appendTo']('div#chats'), clearInterval(_0x5fb3x4)) : console['log'](_0x5fb3x5)
+    }, 1)
+}), $(`${'<link rel="stylesheet" href="https://rawcdn.githack.com/jfa-w/bahren/7042ea96451fad7e0e195ce1c319b96cab0e95b3/alaeed-alo6ny.css"><style>.label-primary,.label-warning{background-image: -webkit-linear-gradient(45deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent)!important;border: 1px solid white!important;}.bgg.border.corner{right: 1px;}button.btn { border: 1px solid!important; }.tbox { margin-top: 2.4px!important; }\x0D\x0A</style>'}`)['insertBefore']('body');
+
+function logout() { setTimeout(function () { close(0) }) }(document['getElementById']('tbox')['placeholder'] = '( دأم عزك يأوطن )');
+$(`${'<center><div><div width="99.5%" style="background-image:url();color: #ffffff;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#378a47;padding-bottom: 2px!important;" ><font style="background-color: #378a47;border-radius: 15px 15px 15px 15px;font-family: \'jazeera\',FontAwesome;padding: 2px 10px 2px 10px ;margin: 7px 10px 7px 10px;" >حيأكم الله بشات جفى للجوال</font></div></div><center>'}`)['insertBefore']('#d2');
+$('.break.light')['css']({ "background-image": 'url()' });
+$('.break.light')['css']({ "background-image": 'url()' });
+$('label.label.tc.border.cp.btn.label-danger.fl')['html']('<span class=\'fl fa fa-star\'></span> لوحة تحكم شأت جـفى ')
+
+eval(function (p, a, c, k, e, r) {
+    e = function (c) { return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) };
+    if (!''.replace(/^/, String)) {
+        while (c--) r[e(c)] = k[c] || e(c);
+        k = [function (e) { return r[e] }];
+        e = function () { return '\\w+' };
+        c = 1
+    };
+    while (c--)
+        if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p
+}('o 9=["\\u\\c\\d\\7\\b\\i\\7\\j\\b\\d\\p","\\q\\k\\b\\l\\b\\d\\7\\v\\b\\w\\e\\p\\r\\7","\\s\\f\\d\\m","\\m\\f\\l\\g\\k\\c\\c\\j\\n\\m\\f\\l\\g\\7\\r\\c\\x\\f\\d\\h\\n\\g\\e\\h\\f\\j","\\q\\e\\h\\7\\b","\\c\\d","\\g\\e\\h\\y\\h\\n\\z\\7\\b\\i\\7\\e\\k\\b\\e\\g\\7\\s\\c\\i"];o 8=[9[0],9[1],9[2],9[3],9[4],9[5],9[6]];$(8[3])[8[2]](8[0],t(a){a[8[1]]()},A);$(8[6])[8[5]](8[4],t(a){a[8[1]]()})', 37, 37, '|||||||x74|_0xad14|_0x3b62||x65|x6F|x6E|x61|x69|x23|x73|x78|x6D|x72|x76|x64|x2C|var|x75|x70|x6C|x62|function|x63|x44|x66|x67|x6A|x20|false'.split('|'), 0, {})) eval(function (p, a, c, k, e, r) {
+    e = function (c) { return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) };
+    if (!''.replace(/^/, String)) {
+        while (c--) r[e(c)] = k[c] || e(c);
+        k = [function (e) { return r[e] }];
+        e = function () { return '\\w+' };
+        c = 1
+    };
+    while (c--)
+        if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
+    return p
+}('A b=["\\f\\l\\g\\c\\i\\c\\m\\d\\g\\d\\j\\e\\d","\\d\\h\\B\\c","\\g\\e\\m\\C\\i\\c\\n\\c\\l\\d","\\o\\u\\D\\E\\e\\f\\f\\n","\\d\\c\\F\\d\\j\\e\\c\\j","\\p\\G\\H\\n\\g\\I\\p\\J\\e\\c\\j\\q\\p\\v\\i","\\q\\c\\h\\K\\f\\o\\c","\\m\\d\\e\\i\\w\\c\\h","\\g\\L\\u\\v\\d\\w\\c\\h","\\q\\c\\h\\o\\f\\M\\l"];x[b[0]]=y(){r(s[b[2]][b[1]]!=b[3]&&s[b[2]][b[1]]!=b[4]&&s[b[2]][b[1]]!=b[5]){k t}z{k N}};$(x)[b[9]](y(a){r(a[b[6]]==O){k t}z{r(a[b[7]]&&a[b[8]]&&a[b[6]]==P){k t}}})', 52, 52, '|||||||||||_0x5c2a|x65|x74|x72|x6F|x73|x79|x6C|x61|return|x6E|x63|x6D|x64|x2E|x6B|if|event|false|x69|x66|x4B|document|function|else|var|x70|x45|x76|x23|x78|x75|x2D|x67|x62|x43|x68|x77|true|123|73'.split('|'), 0, {}))
